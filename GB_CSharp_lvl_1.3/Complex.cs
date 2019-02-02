621c3e9414502a89281467df18d4b64f4aa95243
AVTahion <avkontakt40@gmail.com>
2019-02-02 06:53:10 +0300

    Доделано первое задание пункты б, в.

--- GB_CSharp_lvl_1.3/Complex.cs
+++ GB_CSharp_lvl_1.3/Complex.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-/* 1)   а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры.
+﻿/* 1)   а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры.
         б) Дописать класс Complex, добавив методы вычитания и произведения чисел.Проверить работу класса.
         в) Добавить диалог с использованием switch демонстрирующий работу класса.
     2)  а)  С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке). Требуется подсчитать сумму всех нечётных положительных чисел.
@@ -22,37 +16,55 @@ using System.Threading.Tasks;
 
 namespace GB_CSharp_lvl_1._3
 {
-
-    class Program
+    struct Complex
     {
-        static void Main(string[] args)
+        public double im;
+        public double re;
+
+        public Complex (double im, double re)
         {
-            Complex complex1;
-            complex1.re = 1;
-            complex1.im = 1;
-
-            Complex complex2;
-            complex2.re = 2;
-            complex2.im = 2;
-
-            Complex result = new Complex(0,0);
-
-            Console.WriteLine($"Выберите действие над комплексными числами 1+1i и 2+2i: 1 - для сложения, 2 - для умножения, 3 - для вычитания");
-            int i = Convert.ToByte(Console.ReadLine());
-            switch (i)
-            {
-                case 1: result = complex1.Plus(complex2);
-                    break;
-                case 2: result = complex1.Multi(complex2);
-                    break;
-                case 3: result = complex1.Subtraction(complex2);
-                    break;
-                default:
-                    break;
-            }
-            Console.WriteLine(result.ToString());
-            Console.ReadKey();
+            this.im = im;
+            this.re = re;
         }
-
+        /// <summary>
+        /// Метод складывает комплексные числа.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public Complex Plus(Complex x)
+        {
+            Complex y;
+            y.im = im + x.im;
+            y.re = re + x.re;
+            return y;
+        }
+        /// <summary>
+        /// Метод умножения комплексных чисел.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public Complex Multi(Complex x)
+        {
+            Complex y;
+            y.im = re * x.im + im * x.re;
+            y.re = re * x.re - im * x.im;
+            return y;
+        }
+        /// <summary>
+        /// Метод вычитания комплексных чисел.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public Complex Subtraction(Complex x)
+        {
+            Complex y;
+            y.im = im - x.im;
+            y.re = re - x.re;
+            return y;
+        }
+        public string ToString()
+        {
+            return re + "+" + im + "i";
+        }
     }
 }
